25ed6309739ac67f0b7e4ad16c52f062f32288bf
Shree Viveka M <mineshrajav_bai27@mepcoeng.ac.in>
2026-04-12 19:00:06 +0530

    Add files via upload

--- hashTable/groupAnagrams.java
+++ hashTable/groupAnagrams.java
@@ -1,1 +1,14 @@
-
+class Solution(object):
+    def groupAnagrams(self, strs): # Added self
+        hashmap = {}
+    
+        for word in strs:
+            # Sorting the word creates the unique key for all its anagrams
+            key = ''.join(sorted(word))
+            
+            if key not in hashmap:
+                hashmap[key] = []
+            
+            hashmap[key].append(word)
+    
+        return list(hashmap.values()) # Using .values() is slightly cleaner
